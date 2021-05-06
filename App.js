2ac8c8915f1a4e28a351b68bb028b0d36adbd6b2
ggtenev <georgiy.tenev@gmail.com>
2021-05-06 03:43:24 +0100

    before merge

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import "react-native-gesture-handler";
 
-import * as React from "react";
+import React, { useState,useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { StyleSheet } from "react-native";
 import MainSwitchNavigator from "./DrawerNavigator";
 export default function App() {
+  const [loading,setLoading] = useState(false)
+  const [loggedIn,setLoggedIn] = useState(false)
   return (
     // <Login/>
     <NavigationContainer>
